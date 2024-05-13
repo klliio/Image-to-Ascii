4ba32e893a3936ceaafd84419407a0797cee84ec
klliio <111129234+klliio@users.noreply.github.com>
2024-05-13 23:13:28 +0100

    Fix panic when piping to an early returning program

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use clap::Parser;
+use clap::{error::Result, Parser};
 use image::{self, imageops::FilterType::Nearest, DynamicImage, GenericImageView, Pixel};
-use std::path::PathBuf;
+use std::{
+    io::{self, Stdout, Write},
+    path::PathBuf,
+};
 use termsize;
 
 // Ascii to image converter
@@ -26,36 +29,53 @@ fn main() {
         .unwrap()
         .adjust_contrast(f32::from(args.constrast));
 
+    // unable to get size when piping to a program that ends early like head
     if args.scale == 0 {
-        println!("cols: {}\nrows: {}", termsize::get().unwrap().cols, termsize::get().unwrap().rows);
-        println!("width: {}\nheight: {}", image.width(), image.height());
-        let n_width: u32 = u32::from(termsize::get().unwrap().cols);
-        let n_height: u32 = u32::from(termsize::get().unwrap().rows);
-        image = image.resize(n_width, n_height, Nearest);
-        println!("width: {}\nheight: {}", image.width(), image.height());
+        let size: (u32, u32) = match termsize::get() {
+            Some(size) => (u32::from(size.cols), u32::from(size.rows)),
+            None => {
+                println!("Unable to get terminal size\nDefaulting to 50x50");
+                (50, 50)
+            }
+        };
+        image = image.resize(size.0, size.1, Nearest);
     } else if args.scale != 0 {
         let n_width: u32 =
             (f64::from(image.width()) * (f64::from(args.scale) / 100.0)).round() as u32;
+
         let n_height: u32 =
             (f64::from(image.height()) * (f64::from(args.scale) / 100.0)).round() as u32;
+
         image = image.resize(n_width, n_height, Nearest);
     }
+    
+    if let Err(error) = output(image) {
+        eprintln!("{}", error);
+    }
+}
 
+fn output(image: DynamicImage) -> Result<(), io::Error> {
     let characters: [char; 17] = [
         '$', '#', 'B', '%', '*', 'o', 'c', ';', ':', '<', '~', '^', '"', '\'', ',', '.', ' ',
     ];
+
+    let stdout = io::stdout();
+    let mut stdout = stdout.lock();
 
     let mut i = 1;
     for pix in image.pixels() {
         let luma = pix.2.to_luma()[0];
 
-        print!(
+        write!(
+            stdout,
             "{} ",
             characters[((255 - luma) / u8::try_from(characters.len()).unwrap()) as usize]
         );
         if (i % image.width()) == 0 {
-            println!();
+            write!(stdout, "\n",);
         }
         i += 1;
     }
+
+    Ok(())
 }
