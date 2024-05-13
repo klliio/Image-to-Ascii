232041ddf3bf53ffea6b62594cb7e0bc50ea988f
klliio <111129234+klliio@users.noreply.github.com>
2024-05-14 00:19:30 +0100

    Add support for colours and removing transparent pixels

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use clap::{error::Result, Parser};
 use image::{self, imageops::FilterType::Nearest, DynamicImage, GenericImageView, Pixel};
+use owo_colors::{OwoColorize, Stream::Stdout};
 use std::{
-    io::{self, Stdout, Write},
+    io::{self, Write},
     path::PathBuf,
 };
-use termsize;
 
 // Ascii to image converter
 #[derive(Parser, Debug)]
@@ -13,6 +13,14 @@ struct Args {
     // Path to image
     #[arg(short, long)]
     path: PathBuf,
+
+    // enable colours
+    #[arg(long, default_value_t = false)]
+    colour: bool,
+
+    // remove background
+    #[arg(long, default_value_t = false)]
+    no_bg: bool,
 
     // Contrast [-128 - 127]
     #[arg(short, long, default_value_t = 0)]
@@ -42,19 +50,18 @@ fn main() {
     } else if args.scale != 0 {
         let n_width: u32 =
             (f64::from(image.width()) * (f64::from(args.scale) / 100.0)).round() as u32;
-
         let n_height: u32 =
             (f64::from(image.height()) * (f64::from(args.scale) / 100.0)).round() as u32;
 
         image = image.resize(n_width, n_height, Nearest);
     }
-    
-    if let Err(error) = output(image) {
+
+    if let Err(error) = output(image, args.colour, args.no_bg) {
         eprintln!("{}", error);
     }
 }
 
-fn output(image: DynamicImage) -> Result<(), io::Error> {
+fn output(image: DynamicImage, colour: bool, no_bg: bool) -> Result<(), io::Error> {
     let characters: [char; 17] = [
         '$', '#', 'B', '%', '*', 'o', 'c', ';', ':', '<', '~', '^', '"', '\'', ',', '.', ' ',
     ];
@@ -66,11 +73,23 @@ fn output(image: DynamicImage) -> Result<(), io::Error> {
     for pix in image.pixels() {
         let luma = pix.2.to_luma()[0];
 
-        write!(
-            stdout,
-            "{} ",
-            characters[((255 - luma) / u8::try_from(characters.len()).unwrap()) as usize]
-        );
+        let alpha: u8 = pix.2[3];
+        let mut rgb = image::Rgb([255, 255, 255]);
+        if colour {
+            rgb = pix.2.to_rgb();
+        }
+
+        if (alpha == 0) && (no_bg) {
+            write!(stdout, "  ",);
+        } else {
+            write!(
+                stdout,
+                "{} ",
+                characters[((255 - luma) / u8::try_from(characters.len()).unwrap()) as usize]
+                    .if_supports_color(Stdout, |text| text.truecolor(rgb[0], rgb[1], rgb[2]))
+            );
+        }
+
         if (i % image.width()) == 0 {
             write!(stdout, "\n",);
         }
